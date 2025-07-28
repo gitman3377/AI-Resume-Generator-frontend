39bcdf10ee4fbe43417f8104026f944cc8cd0a84
gitman3377 <manmohan05012@gmail.com>
2025-07-28 21:01:14 +1000

    sidebar design and modules has been done

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { mainmenu } from './sidebar_menu';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit{
+
+  public href:string = "";
+
+  mainmenu:any = mainmenu;
+  activeMenu:any;
+
+constructor(
+  public router: Router
+)
+{}
+
+ngOnInit() {
+  
+}
+
+ngDoCheck(){
+  this.activeMenu = this.removespecialcharacters(this.router.url);
+  this.href = this.router.url;
+
+  let all = this.href.split('/');
+  this.activeMenu = all[1];
+}
+
+removespecialcharacters(word:string){
+  return word.replace(/[^a-zA-Z0-9\s]/g, '');
+}
 
 }
---
+++ src/app/shared/components/sidebar/sidebar_menu.ts
@@ -0,0 +1,32 @@
+import path from "node:path";
+import { title } from "node:process";
+
+export const mainmenu = 
+[
+    {
+    path: "home",
+    title: "Home",
+    extralink: true,
+    class: "",
+    categoryId: 1,
+    icon: "fa-solid fa-house"
+    },
+
+    {
+    path: "build",
+    title: "Build Resume",
+    extralink: true,
+    class: "",
+    categoryId: 1,
+    icon: "fa-duotone fa-solid fa-gears"
+    },
+
+    {
+    path: "myresume",
+    title: "My Resume",
+    extralink: true,
+    class: "",
+    categoryId: 1,
+    icon: "fa-solid fa-file"
+    }
+]
